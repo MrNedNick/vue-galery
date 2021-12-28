2ee4b0a9d8ed525d673f064b86746a283ffc9504
MrNedNick <mrnediala@gmail.com>
2021-12-28 19:45:16 +0100

    First commit

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-col cols="4">
+        <v-card @click="openPhoto">
+            <v-card-title>{{photo.title}}</v-card-title>
+            <v-card-text>
+                <v-img
+                    width='200'
+                    heigth='200'
+                    :src='photo.url'
+                />
+            </v-card-text>
+        </v-card>
+        {{photo.id + photo.title}}
+    </v-col>
+</template>
+
+<script>
+export default {
+    props: {
+        photo: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        openPhoto() {
+            this.$emit('openPhoto', this.photo)
+        }
+    }
+}
+</script>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+     <v-container>
+        MAIN
+     </v-container>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
